03c65b02ba6f201782641e52ebe59ea1b21fcea4
longyuqi <longyuqi@tencent.com>
2023-03-02 21:56:27 +0800

    feat: add gpt and some markdown parse

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+import axios from "axios";
+import { marked } from 'marked';
+import hljs from 'highlight.js';
+// import commonmark from 'commonmark';
+
+const question = ref<string>("");
+const answer = ref<string>("empty");
+
+const AUTH_TOKEN = "Bearer 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";
+
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+marked.setOptions({
+  highlight: function(code) {
+    return hljs.highlightAuto(code).value;
+  }
+});
+
+function send() {
+
+  var reader = new commonmark.Parser();
+      var writer = new commonmark.HtmlRenderer();
+      var parsed = reader.parse("\n\n当一个函数被触发多次时，防抖函数可以确保它只在最后一次触发后才被执行。下面是一个简单的防抖函数的实现：\n\n```\nfunction debounce(func, wait) {\n  let timeout;\n  return function() {\n    const context = this;\n    const args = arguments;\n    const later = function() {\n      timeout = null;\n      func.apply(context, args);\n    };\n    clearTimeout(timeout);\n    timeout = setTimeout(later, wait);\n  };\n}\n```\n\n在这个函数中，我们定义了一个 `timeout` 变量来存储 `setTimeout` 返回的 ID。当防抖函数被调用时，它会清除当前的定时器，并使用 `setTimeout` 来设置一个新的定时器，这个定时器将在指定的时间间隔后调用传入的函数。 如果在新的定时器之前，防抖函数被调用了多次，那么之前的定时器会被清除，只有最后一次调用的函数会被执行。\n\n使用方法如下：\n\n```\nfunction myFunc() {\n  console.log('Hello world!');\n}\n\nconst debouncedFunc = debounce(myFunc, 200);\n\ndebouncedFunc(); // 在200毫秒后执行\ndebouncedFunc(); // 取消上一次并在200毫秒后执行\ndebouncedFunc(); // 取消上一次并在200毫秒后执行\n```"); // parsed is a 'Node' tree
+      // transform parsed if you like...
+      var html = writer.render(parsed); // result is a String
+      document.getElementById("answer").innerHTML = html;
+  // axios.post('https://api.openai.com/v1/chat/completions', {
+  //   "model": "gpt-3.5-turbo",
+  //   "messages": [{ "role": "user", "content": question.value }]
+  // })
+  //   .then(function (response) {
+  //     console.log(response);
+  //     answer.value = response.data.choices[0].message.content;
+  //     // const html = marked.parse(answer.value);
+  //     // console.log(answer.value);
+  //     // console.log(html);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="div">
+    <textarea id="question" v-model="question"></textarea>
+    <button @click="send()"> send</button>
+  </div>
+  <div>
+    <div id="answer"></div>
+    <!-- <textarea id="answer" :value="answer"></textarea> -->
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+#question {
+  height: 500px;
+  width: 500px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#answer {
+  height: 1000px;
+  width: 500px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#div {
+  display: flex;
+  flex-direction: column;
 }
 </style>
